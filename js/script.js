6f7bb32527e65dde6c4f577db5c60e57c9cb9be1
default <trisha.ray@stu.ocsb.ca>
2025-03-31 17:17:45 +0000

    template

--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
+// Created by: Trisha Ray
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+"use strict"
+
+function calculateAreaOfTrapizoid () {
+  // input
+  const abase = parseInt(document.getElementById('a-base').value);
+  const bbase = parseInt(document.getElementById('b-base').value);
+  const height = parseInt(document.getElementById('height').value);
+
+  // process
+  const areaOfTrapizoid = [(abase + bbase) / 2] * height
+
+  // output
+  document.getElementById('area').innerHTML = 'Area is: ' + areaOfTrapizoid + ' mm²'
+}
